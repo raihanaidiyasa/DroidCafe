ea283466bb110050248aed6321a14c601d0a3a0d
raihanaidiyasa <raihanaidiyasa25@gmail.com>
2024-10-07 18:04:00 +0700

    Initial commit project Droid Cafe

---
+++ app/src/main/java/com/example/droidcafe/OrderActivity.kt
@@ -0,0 +1,40 @@
+package com.example.droidcafe
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order)
+    }
+    fun onRadioButtonClicked(view: View) {
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+            R.id.sameday -> if (checked)
+                displayToast(getString(R.string.same_day_messenger_service))
+
+            R.id.nextday -> if (checked)
+                displayToast(getString(R.string.next_day_ground_delivery))
+
+            R.id.pickup -> if (checked)
+                displayToast(getString(R.string.pick_up))
+
+            else -> {}
+        }
+    }
+    private fun displayToast(message: String) {
+        Toast.makeText(
+            applicationContext, message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
